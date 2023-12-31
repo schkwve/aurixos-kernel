906ece98ec24da17b0d97033157b69630463da2f
Jozef Nagy <schkwve@gmail.com>
2023-12-31 22:11:06 +0100

    Added file header comments

--- kernel/arch/aarch64/include/arch/io/uart.h
+++ kernel/arch/aarch64/include/arch/io/uart.h
@@ -1,3 +1,29 @@
+/************************************************************************
+ * FILENAME:         uart.h
+ *
+ * DESCRIPTION:
+ *       UART-related Raspberry Pi 4 (aarch64) specific addresses
+ *       definitions and function declarations.
+ *
+ * AUTHOR:     Jozef Nagy                  DATE:    30 Dec 2023
+ *
+ * COPYRIGHT:
+ *       Copyright (C) 2023  Jozef Nagy.
+ *
+ *       This program is free software; you can redistribute it and/or
+ *       modify it under the terms of the GNU General Public License
+ *       as published by the Free Software Foundation; either version 2
+ *       of the License, or (at your option) any later version.
+ *       
+ *       This program is distributed in the hope that it will be useful,
+ *       but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *       GNU General Public License for more details.
+ *       
+ *       You should have received a copy of the GNU General Public License
+ *       along with this program; If not, see <http://www.gnu.org/licenses/>.
+ **/
+
 #ifndef AARCH64_UART_H
 #define AARCH64_UART_H
 
--- kernel/mm/mmio.c
+++ kernel/mm/mmio.c
@@ -1,3 +1,28 @@
+/************************************************************************
+ * FILENAME:         mmio.c
+ *
+ * DESCRIPTION:
+ *       MMIO read/write function declarations.
+ *
+ * AUTHOR:     Jozef Nagy                  DATE:    30 Dec 2023
+ *
+ * COPYRIGHT:
+ *       Copyright (C) 2023  Jozef Nagy.
+ *
+ *       This program is free software; you can redistribute it and/or
+ *       modify it under the terms of the GNU General Public License
+ *       as published by the Free Software Foundation; either version 2
+ *       of the License, or (at your option) any later version.
+ *       
+ *       This program is distributed in the hope that it will be useful,
+ *       but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *       GNU General Public License for more details.
+ *       
+ *       You should have received a copy of the GNU General Public License
+ *       along with this program; If not, see <http://www.gnu.org/licenses/>.
+ **/
+
 #include <mm/mmio.h>
 
 #include <stddef.h>
